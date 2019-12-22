259952131f4c790ced7c2dfa6b27636272d4dda3
TungNguyen <tungnm52@wru.vn>
2019-12-22 17:41:38 +0700

    update

--- app.js
+++ app.js
@@ -18,14 +18,6 @@ const app = express();
 //     }
 // }));
 
-
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
 app.use(cors({ origin: ['http://localhost:3000', 'https://originalphotos.herokuapp.com'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
