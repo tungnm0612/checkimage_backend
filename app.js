2d96a6092b242984a84355062aa9aedfdc4589a3
TungNguyen <tungnm52@wru.vn>
2019-12-22 16:43:01 +0700

    update

--- app.js
+++ app.js
@@ -56,30 +56,22 @@ app.use(
     // }
 });
 
+app.use(function (req, res, next) { 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); 
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization'); 
+    res.setHeader('Access-Control-Allow-Credentials', true); 
+    if ('OPTIONS' === req.method) { 
+        res.send(204); 
+    } else { 
+        next(); 
+    } 
+}); 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://originalphotos.herokuapp.com"); // update to match the domain you will make the request from
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   });
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://originalphotos.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 // app.use(cors({ origin: ['http://localhost:3000', 'https://originalphotos.herokuapp.com'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
